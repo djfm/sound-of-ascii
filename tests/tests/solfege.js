5c5201362b777285d2296aa9ec4eee55bf2d69fb
fram <fm.de.jouvencel@gmail.com>
2015-04-10 19:42:23 +0200

    Simple note parsing and conversion to semitones in the C major scale

--- tests/tests/solfege.js
+++ tests/tests/solfege.js
@@ -1,9 +1,30 @@
 /* global define, describe, it */
 
-define([], function () {
-    describe('Solfege', function () {
-        it('a dummy test should pass', function () {
+define(['underscore', 'lib/solfege'], function (_, solfege) {
 
+    var noteToSemitones = solfege.noteToSemitones;
+
+    describe('Solfege', function () {
+
+        var strAndExpectedSemitones = [
+            ['c'    , 0 ],
+            ['c0'   , 0 ],
+            ['a0'   , 9 ],
+            ['c1'   , 12],
+            ['c#'   , 1 ],
+            ['c+'   , 1 ],
+            ['c♯'   , 1 ],
+            ['d♭'   , 1 ],
+            ['db'   , 1 ],
+            ['d-'   , 1 ],
+            ['g4'   , 55]
+        ];
+
+        _.each(strAndExpectedSemitones, function (strAndExpectedSemitone) {
+            var str = strAndExpectedSemitone[0], semitone = strAndExpectedSemitone[1];
+            it('`' + str + '` should have semitone `' + semitone + '`', function () {
+                noteToSemitones(str).should.equal(semitone);
+            });
         });
     });
 });
