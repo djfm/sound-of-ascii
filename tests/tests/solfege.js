9d8446dbb0e707e02411933a665c297d70b60b1d
fram <fm.de.jouvencel@gmail.com>
2015-04-11 10:11:41 +0200

    Created Song, should be playable soon

--- tests/tests/solfege.js
+++ tests/tests/solfege.js
@@ -3,6 +3,7 @@
 define(['underscore', 'lib/solfege'], function (_, solfege) {
 
     var noteToSemitones = solfege.noteToSemitones;
+    var semitonesToFreq = solfege.semitonesToFreq;
 
     describe('Solfege', function () {
 
@@ -24,6 +25,19 @@ define(['underscore', 'lib/solfege'], function (_, solfege) {
             var str = strAndExpectedSemitone[0], semitone = strAndExpectedSemitone[1];
             it('`' + str + '` should have semitone `' + semitone + '`', function () {
                 noteToSemitones(str).should.equal(semitone);
+            });
+        });
+
+        var semitonesAndExpectedFreqs = [
+            [0   ,   16.35],
+            [12  ,   32.70],
+            [13  ,   34.64]
+        ];
+
+        _.each(semitonesAndExpectedFreqs, function (semitonesAndExpectedFreq) {
+            var semitone = semitonesAndExpectedFreq[0], freq = semitonesAndExpectedFreq[1];
+            it('`' + semitone + '` should have freq `' + freq + '`', function () {
+                semitonesToFreq(semitone).should.equal(freq);
             });
         });
     });
--- tests/tests/song.js
+++ tests/tests/song.js
@@ -12,7 +12,16 @@ define(['lib/pattern', 'lib/song', 'chai'], function (patternLib, songLib, chai)
         });
         it('should be made of tracks', function () {
             var song = new Song(new Pattern('atom', 'c'));
-            song.tracks().length.should.equal(1);
+            song.tracks.length.should.equal(1);
+        });
+        it('should be made of tracks that contain degrees, that contain slots, that contain notes... phew :)', function () {
+            var song = new Song(new Pattern('atom', 'g#4'));
+            song.tracks.length.should.equal(1);
+            song.tracks[0].degrees[0].slots[0].notes[0].should.include({
+                name: 'g#4',
+                value: 56,
+                freq: 415.36
+            });
         });
     });
 });
